77404b230ecb9751377d82afa96a9bb4b2affde2
hsl0 <hsl0@outlook.kr>
2022-07-12 05:17:49 +0900

    change into cjs module

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-import { createFilter } from '@rollup/pluginutils';
-import MagicString from 'magic-string';
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const pluginutils_1 = require("@rollup/pluginutils");
+const magic_string_1 = __importDefault(require("magic-string"));
 const regex = /\/\*+\s*Copyright\s\(c\)\sMicrosoft\sCorporation\.[\w./\s",]+\*+\s?\*\//gm;
-export default (function notMicrosoft(options) {
-    const filter = createFilter(options === null || options === void 0 ? void 0 : options.include, options === null || options === void 0 ? void 0 : options.exclude);
+exports.default = (function notMicrosoft(options) {
+    const filter = (0, pluginutils_1.createFilter)(options === null || options === void 0 ? void 0 : options.include, options === null || options === void 0 ? void 0 : options.exclude);
     return {
         name: 'not-microsoft',
         transform(code, id) {
             if (filter(id)) {
-                const ms = new MagicString(code);
+                const ms = new magic_string_1.default(code);
                 ms.replace(regex, '');
                 return {
                     code: ms.toString(),
