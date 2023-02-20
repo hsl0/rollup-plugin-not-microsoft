af173f81202450ec1404b3e361e84efc9d69108d
hsl0 <hsl0@outlook.kr>
2023-02-21 00:56:55 +0900

    use eslint and prettier

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import type { Plugin, PluginImpl } from 'rollup';
-import { createFilter, FilterPattern } from '@rollup/pluginutils';
+//import { createFilter, FilterPattern } from '@rollup/pluginutils';
 import MagicString from 'magic-string';
 
 const regex =
@@ -8,7 +8,7 @@ const regex =
 export default <PluginImpl>function notMicrosoft(): Plugin {
     return {
         name: 'not-microsoft',
-        transform(code, id) {
+        transform(code /*, id*/) {
             const ms = new MagicString(code);
             ms.replace(regex, '');
             return {
